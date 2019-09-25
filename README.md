810334b30bde703fea41170a9e100b5c9604f216
Gabriel Viganotti <gabrielviganotti@gmail.com>
2019-09-25 18:31:34 -0300

    Add some more example calls and upgrade safe-nodejs to v0.2.4

--- README.md
+++ README.md
@@ -16,17 +16,17 @@ You can learn more about each of these components within the [Quick Start Guide]
 
 ## To Use
 
-To clone and run this repository you'll need [Git](https://git-scm.com) and [Node.js](https://nodejs.org/en/download/) (which comes with [npm](http://npmjs.com)) installed on your computer. From your command line:
-
 ```bash
 # Clone this repository
-git clone https://github.com/electron/electron-quick-start
+git clone https://github.com/bochaco/electron-neon-safe.git
 # Go into the repository
-cd electron-quick-start
+cd electron-neon-safe
 # Install dependencies
-npm install
+yarn
+# Rebuild native modules
+yarn build-electron
 # Run the app
-npm start
+yarn start
 ```
 
 Note: If you're using Linux Bash for Windows, [see this guide](https://www.howtogeek.com/261575/how-to-run-graphical-linux-desktop-applications-from-windows-10s-bash-shell/) or use `node` from the command prompt.
--- renderer.js
+++ renderer.js
@@ -6,7 +6,7 @@ const APP_ID = "net.maidsafe.safe_browser";
 const APP_NAME = "SAFE Browser";
 const APP_VENDOR = "MaidSafe.net Ltd";
 
-const { Safe } = require('safe-nodejs');
+const { Safe, XorUrlEncoder, SafeDataType, SafeContentType } = require('safe-nodejs');
 
 let safe = new Safe();
 
@@ -15,6 +15,24 @@ let auth_credentials = safe.auth_app(APP_ID, APP_NAME, APP_VENDOR);
 
 console.log("Connecting to the Network...");
 safe.connect("net.maidsafe.safe-nodejs", auth_credentials);
+
+// XorUrlEncoder
+let random = Math.floor(Math.random() * Math.floor(1000));
+let nrs_url = `safe://mypubname-${random}`;
+console.log("Let's parse from NRS URL:", nrs_url);
+let xorname = Uint8Array.from([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2]);
+let xorurl_encoder = new XorUrlEncoder(xorname.buffer, 10000, SafeDataType.PublishedSeqAppendOnlyData, SafeContentType.FilesContainer, "/folder", ["sub1", "sub2"], 5);
+console.log("XOR-URL generated:", xorurl_encoder);
+console.log("Encoding version:", xorurl_encoder.encoding_version());
+console.log("Xorname:", xorurl_encoder.xorname());
+console.log("Type tag:", xorurl_encoder.type_tag());
+console.log("Data type:", xorurl_encoder.data_type());
+console.log("Content type:", xorurl_encoder.content_type());
+console.log("Path:", xorurl_encoder.path());
+console.log("Subnames:", xorurl_encoder.sub_names());
+console.log("Content version:", xorurl_encoder.content_version());
+console.log("XOR-URL string:", xorurl_encoder.to_string());
+console.log("XOR-URL base64:", xorurl_encoder.to_base("base64"));
 
 // FilesContainer create
 console.log("Creating FilesContainer...");
@@ -31,6 +49,19 @@ safe.files_container_sync("testfolder/subfolder", files_container[0], false, fal
 safe_data = safe.files_container_get(files_container[0]);
 console.log("FilesContainer synced: ", safe_data);
 
+// FilesContainer add
+console.log("Adding a file to FilesContainer...");
+safe.files_container_add("testfolder/test.md", `${files_container[0]}/new-test.md`, false, false, false, false);
+safe_data = safe.files_container_get(files_container[0]);
+console.log("FilesContainer updated: ", safe_data);
+
+// FilesContainer add from raw bytes
+console.log("Adding a file from raw bytes to FilesContainer...");
+let raw_bytes = Buffer.from('Raw bytes of the files I added');
+safe.files_container_add_from_raw(raw_bytes, `${files_container[0]}/from-raw.md`, false, false, false, false);
+safe_data = safe.files_container_get(files_container[0]);
+console.log("FilesContainer updated: ", safe_data);
+
 // PublishedImmutableData put
 console.log("Putting PublishedImmutableData...");
 let buffer = Uint8Array.from([72, 101, 108, 108, 111, 32, 69, 108, 101, 99, 116, 114, 111, 110, 33, 33, 33, 10]);
@@ -42,8 +73,16 @@ console.log("Let's fetch PublishedImmutableData from ", immd_url);
 safe_data = safe.files_get_published_immutable(immd_url);
 console.log("Fetched PublishedImmutableData content: ", safe_data);
 
+
+buffer = Buffer.from('Using Buffer.from');
+let idUrl = safe.files_put_published_immutable( buffer );
+
+console.log("Let's fetch PublishedImmutableData from ", idUrl);
+safe_data = safe.files_get_published_immutable(idUrl);
+console.log("Fetched PublishedImmutableData content: ", safe_data);
+
 // Create an NRS public name
-let random = Math.floor(Math.random() * Math.floor(1000));
+random = Math.floor(Math.random() * Math.floor(1000));
 let nrs_name = `mypubname-${random}`;
 console.log("Let's link NRS name to ", files_container[0]);
 let nrs_map_data = safe.nrs_map_container_create(nrs_name, `${files_container[0]}?v=0`, true, true, false);
